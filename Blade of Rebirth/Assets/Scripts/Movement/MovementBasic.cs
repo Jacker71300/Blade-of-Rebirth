1f4275e3c97a3718b1efd07cfb459ce786b8c6ea
sj3603 <56056374+sj3603@users.noreply.github.com>
2021-02-20 00:51:25 -0500

    Player movement and basic camera done
    Implemented basic player movement and 3rd person camera using cinemachine and new unity input system, used modified template third person player script from unity docs

--- Blade of Rebirth/Assets/Scripts/Movement/MovementBasic.cs
+++ Blade of Rebirth/Assets/Scripts/Movement/MovementBasic.cs
@@ -1,22 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class MovementBasic : MonoBehaviour
 {
-    float accel;
+    [SerializeField]
+    private InputActionReference movementControl;
+    [SerializeField]
+    private InputActionReference jumpControl;
+    [SerializeField]
+    private float playerSpeed = 2.0f;
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+    [SerializeField]
+    private float rotationSpeed = 4f;
 
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private Transform cameraMainTransform;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        accel = 5.0f;
-        //gameObject.GetComponent<Rigidbody>().velocity = direction * dashSpeed;
+        movementControl.action.Enable();
+        jumpControl.action.Enable();
     }
 
-    // Update is called once per frame
+    private void OnDisable()
+    {
+        movementControl.action.Disable();
+        jumpControl.action.Disable();
+    }
+
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        cameraMainTransform = Camera.main.transform;
+    }
+
     void Update()
     {
-        //gameObject.GetComponent<Rigidbody>().velocity += gameObject.GetComponent<PlayerInput>();
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = movementControl.action.ReadValue<Vector2>();
+        Debug.Log(movement);
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
+        move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
+        move.y = 0f;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Changes the height position of the player..
+        if (jumpControl.action.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if (movement != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMainTransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
     }
 }
